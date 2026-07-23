ed6c166a5a6601e5e8b24a98cc3bef7ad94d96af
dawgdevv <nraj02415@gmail.com>
2026-07-23 17:02:31 +0530

    chore: add process import to Vite configuration for improved compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
+import process from "node:process";
 
 const plugins = [react()];
 
